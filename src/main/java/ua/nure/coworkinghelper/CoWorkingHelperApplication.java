e7823e8e8bc200258c5eb72b4cfda15a836124f0
deyasix <akikidzhan@gmail.com>
2022-11-11 14:36:13 +0200

    create the project

---
+++ src/main/java/ua/nure/coworkinghelper/CoWorkingHelperApplication.java
@@ -0,0 +1,13 @@
+package ua.nure.coworkinghelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoWorkingHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoWorkingHelperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/nure/coworkinghelper/CoWorkingHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.nure.coworkinghelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoWorkingHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
